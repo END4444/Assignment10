050200f28a47d9aeea97182f9cbca231fe0c84c8
u0926460 <71471282+u0926460@users.noreply.github.com>
2023-07-19 13:19:15 -0600

    Add files via upload

--- BinaryMaxHeap.java
+++ BinaryMaxHeap.java
@@ -3,29 +3,59 @@ package assignment10;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
-
+/**
+ * Creates a binary max heap where the first node is the maximum and each child is less then its parents
+ * implements the PriorityQueue interface
+ *
+ * @author Austin Allen and Ethan Doman
+ * @version July 18, 2023
+ *
+ * @param <E>
+ */
 public class BinaryMaxHeap <E> implements PriorityQueue<E>{
     private E[] backingArray;
     int size;
     int startingArraySize =10;
     Comparator<? super E> cmp;
+
+    /**
+     * Constructor with no arguements
+     */
     @SuppressWarnings("unchecked")
     public BinaryMaxHeap(){
 
         backingArray = (E[]) new Object[startingArraySize];
         size = 0;
     }
+    /**
+     * Constructor for a custom comparator
+     *
+     * @param cmp
+     */
     @SuppressWarnings("unchecked")
     public BinaryMaxHeap(Comparator<? super E> cmp){
         this.cmp=cmp;
         backingArray = (E[]) new Object[startingArraySize];
         size = 0;
     }
+
+    /**
+     * Constructor for building a max heap given a list already in order
+     *
+     * @param list
+     */
     @SuppressWarnings("unchecked")
     public BinaryMaxHeap(List<? extends E> list){
         size = list.size();
         buildHeap(list);
     }
+
+    /**
+     * Constructor for building a max heap given a list and a custom comparator
+     *
+     * @param list
+     * @param cmp
+     */
     @SuppressWarnings("unchecked")
     public BinaryMaxHeap(List<? extends E> list, Comparator<? super E> cmp){
         this.cmp = cmp;
@@ -34,6 +64,12 @@ public class BinaryMaxHeap <E> implements PriorityQueue<E>{
         buildHeap(list);
     }
 
+    /**
+     * Adds the given item to this priority queue.
+     * O(1) in the average case, O(log N) in the worst case
+     *
+     * @param item
+     */
     @Override
     public void add(E item) {
         if(size == backingArray.length - 1)
@@ -44,6 +80,13 @@ public class BinaryMaxHeap <E> implements PriorityQueue<E>{
 
     }
 
+    /**
+     * Returns, but does not remove, the maximum item this priority queue.
+     * O(1)
+     *
+     * @return the maximum item
+     * @throws NoSuchElementException if this priority queue is empty
+     */
     @Override
     public E peek() throws NoSuchElementException {
         if (backingArray[1]==null)
@@ -51,6 +94,13 @@ public class BinaryMaxHeap <E> implements PriorityQueue<E>{
         return backingArray[1];
     }
 
+    /**
+     * Returns and removes the maximum item this priority queue.
+     * O(log N)
+     *
+     * @return the maximum item
+     * @throws NoSuchElementException if this priority queue is empty
+     */
     @Override
     public E extractMax() throws NoSuchElementException {
         E maxItem = this.peek();
@@ -64,15 +114,28 @@ public class BinaryMaxHeap <E> implements PriorityQueue<E>{
         return maxItem;
     }
 
+    /**
+     * Returns the number of items in this priority queue.
+     * O(1)
+     */
     @Override
     public int size() {
         return this.size;
     }
 
+    /**
+     * Returns true if this priority queue is empty, false otherwise.
+     * O(1)
+     */
     @Override
     public boolean isEmpty() {
         return this.size==0;
     }
+
+    /**
+     * Empties this priority queue of items.
+     * O(1)
+     */
     @SuppressWarnings("unchecked")
     @Override
     public void clear() {
@@ -80,6 +143,15 @@ public class BinaryMaxHeap <E> implements PriorityQueue<E>{
         size = 0;
     }
 
+    /**
+     * Creates and returns an array of the items in this priority queue,
+     * in the same order they appear in the backing array.
+     * O(N)
+     *
+     * (NOTE: This method is needed for grading purposes. The root item
+     * must be stored at index 0 in the returned array, regardless of
+     * whether it is in stored there in the backing array.)
+     */
     @Override
     public Object[] toArray() {
         Object[] newArray = new Object[size];
@@ -87,6 +159,12 @@ public class BinaryMaxHeap <E> implements PriorityQueue<E>{
             newArray[i-1] = backingArray[i];
         return newArray;
     }
+
+    /**
+     * Private helper method fro building the heap using an ordered list.
+     *
+     * @param list
+     */
     @SuppressWarnings("unchecked")
     private void buildHeap(List<? extends E> list){
          backingArray = (E[]) new Object[list.size()+1];
@@ -98,12 +176,24 @@ public class BinaryMaxHeap <E> implements PriorityQueue<E>{
 
     }
 
+    /**
+     * Finds the position the item should be at by starting at the bottom.
+     *
+     * @param item
+     * @param i
+     */
     private void percolateUp(E item, int i){
         for( backingArray[0]=item; innerCompare(item, backingArray[i/2]) < 0; i /= 2)
             backingArray[i] = backingArray[i/2];
         backingArray[i] = item;
         backingArray[0] = null;
     }
+
+    /**
+     * Finds the position the item should be at by starting at the top.
+     *
+     * @param i
+     */
     private void percolateDown( int i){
         int child;
         int loc = i;
@@ -121,6 +211,14 @@ public class BinaryMaxHeap <E> implements PriorityQueue<E>{
 
         backingArray[loc] = temp;
     }
+
+    /**
+     * Private helper method for comparing to elements.
+     *
+     * @param lhs
+     * @param rhs
+     * @return
+     */
     @SuppressWarnings("unchecked")
     private int innerCompare(E lhs, E rhs)
     {
@@ -130,6 +228,12 @@ public class BinaryMaxHeap <E> implements PriorityQueue<E>{
             return cmp.compare(rhs,lhs);
         return ((Comparable<? super E>)rhs).compareTo(lhs);
     }
+
+    /**
+     * Private helper method for increasing the size of the backing array.
+     *
+     * @param newSize
+     */
     @SuppressWarnings("unchecked")
     private void growArray(int newSize){
         E[] tempArray = (E[]) new Object[newSize];
